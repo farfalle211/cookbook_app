510ea42c4726db3abb0a2f809400346834c9a311
farfalle211 <farfalle211@gmail.com>
2019-01-18 12:32:28 -0600

    restful views

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -4,11 +4,6 @@ class Api::RecipesController < ApplicationController       #namespace
     render 'index.json.jbuilder'
   end
 
-  def show
-    recipe_id = params[:id]     #you can put params[:id] in place of recipe_id on the next line
-    @recipe = Recipe.find(recipe_id)
-    render 'show.json.jbuilder'
-  end
 
   def create
     @recipe = Recipe.new(
@@ -21,4 +16,32 @@ class Api::RecipesController < ApplicationController       #namespace
     @recipe.save
     render 'show.json.jbuilder'   #if we want to display one, we use the show view. 
   end
+
+  def show
+    recipe_id = params[:id]     #you can put params[:id] in place of recipe_id on the next line
+    @recipe = Recipe.find(recipe_id)
+    render 'show.json.jbuilder'
+  end
+
+  def update    #change a preexisting recipe
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.title = params[:title] || @recipe.title
+    @recipe.chef = params[:chef] || @recipe.chef
+    @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+    @recipe.directions = params[:directions] || @recipe.directions
+    @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+    @recipe.image_url = params[:irmage_url] || @recipe.image_url
+
+    @recipe.save
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    render json: {message: "Successfully removed recipe"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
       get '/recipes' => 'recipes#index'   #recipes must be plural according to restful routing conventions
-      get '/recipes/:id' => 'recipes#show'  #show displays a single photo, must have a wildcard :id
       post '/recipes' => 'recipes#create'
+      get '/recipes/:id' => 'recipes#show'  #show displays a single photo, must have a wildcard :id
+      patch '/recipes/:id' => 'recipes#update'
+      delete '/recipes/:id' => 'recipes#destroy'
+
   end
 end
