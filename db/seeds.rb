06d96d24fc0a8eb0734f61c4f2d01fc33c34e504
farfalle211 <farfalle211@gmail.com>
2019-03-01 09:30:01 -0600

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Recipe.create!([
+  {title: "Ratatouille", chef: "Remy", ingredients: "1 Large Zucchini, 1 Large Yellow Summer Squash, 2 Small Eggplants, 1 Red Bell Pepper, 1/2 Small Yellow Onion, 2 Garlic Cloves, 15 oz. can Crushed Tomatoes, Fresh Thyme, 1 tsp Salt", directions: "mix them", image_url: "https://media.chefdehome.com/740/0/0/ratatouille/ratatouille-casserole.jpg", prep_time: 43},
+  {title: "Jelly Beans", chef: "Toot Sweet", ingredients: "sugar, sugar, sugar", directions: "mix, compact into candy pill form, rot teeth", image_url: "https://cdn-tp1.mozu.com/9046-11441/cms/11441/files/5863e932-40ab-4982-ae5a-67f79a62f724?max=300", prep_time: 15},
+  {title: "baked beans", chef: "Frank Bush", ingredients: "can, beans, fire", directions: "out of the can, into the man ", image_url: "https://www.simplyrecipes.com/wp-content/uploads/2011/03/boston-baked-beans-vertical-a-1200.jpg", prep_time: 1},
+  {title: "Christmas Candy", chef: "Santa", ingredients: "candy, chocolate", directions: "unwrap", image_url: "https://www.chelseasmessyapron.com/wp-content/uploads/2016/10/candy-cane-hearts.jpg", prep_time: 15},
+  {title: "BUBBLES", chef: "Mr. Bubbles", ingredients: "soap, water", directions: "put soap in tub, swish", image_url: "https://financialpostcom.files.wordpress.com/2017/12/1222bubbles.jpg?quality=80&strip=all&w=412", prep_time: 15}
+])
